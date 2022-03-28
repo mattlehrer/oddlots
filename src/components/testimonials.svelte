ef1f47e1a18c7bea7c46d271275c4feede7e8c7d
Matt Lehrer <lehrerm@gmail.com>
2022-03-28 13:07:26 +0200

    mobile layout, k_pendergrast

--- src/components/testimonials.svelte
+++ src/components/testimonials.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import KPendergrast from '$lib/tweets/k_pendergrast.svelte';
 	import NicCarter from '$lib/tweets/nic_carter.svelte';
 	import SamAntar from '$lib/tweets/sam_antar.svelte';
 	import ScottIrwin from '$lib/tweets/scott_irwin.svelte';
 </script>
 
 <!-- This example requires Tailwind CSS v2.0+ -->
-<ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mx-16">
+<ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mx-4 sm:mx-16">
 	<li class="col-span-1 bg-white rounded-lg  divide-y divide-gray-200">
 		<SamAntar />
 	</li>
@@ -14,6 +15,9 @@
 	</li>
 	<li class="col-span-1 bg-white rounded-lg  divide-y divide-gray-200">
 		<ScottIrwin />
+	</li>
+	<li class="col-span-1 bg-white rounded-lg  divide-y divide-gray-200">
+		<KPendergrast />
 	</li>
 
 	<!-- More people... -->
---
+++ src/lib/tweets/k_pendergrast.svelte
@@ -0,0 +1,11 @@
+<blockquote class="twitter-tweet">
+	<p lang="en" dir="ltr">
+		Hearing people talk about Odd Lots i assumed it was like a dirtbag-adjacent podcast but the
+		promo image is THIS? lol! i don&#39;t care if it&#39;s good, i&#39;m not listening to those
+		smirks <a href="https://t.co/uB52y1bteO">pic.twitter.com/uB52y1bteO</a>
+	</p>
+	&mdash; kelly p. (@k_pendergrast)<a
+		href="https://twitter.com/k_pendergrast/status/1507822219665149952?ref_src=twsrc%5Etfw"
+		>March 26, 2022</a
+	>
+</blockquote>
